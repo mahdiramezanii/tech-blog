ac805f0ccb8d7a96ac6c35572531d1a502164b20
mahdiramazani <mahdiramazani1281@gmail.com>
2023-07-29 22:45:03 +0330

    add splach screen

--- lib/my_colors.dart
+++ lib/my_colors.dart
@@ -4,6 +4,5 @@ class SolidCollrs{
 
   static const Color primaryCollr=Colors.black;
   static const Color headTitelColor=Colors.white;
-  
 
 }
---
+++ lib/splash-Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplachScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return const Scaffold(
+        body: ,
+    );
+  }
+
+
+}
