24b5bea2833ec4297ae5f689d1482e5e27113f5b
mahdiramazani <mahdiramazani1281@gmail.com>
2023-07-29 22:48:56 +0330

    add a image in splach screen

--- lib/splash-Screen.dart
+++ lib/splash-Screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tech_blog/gen/assets.gen.dart';
 
 class SplachScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     
-    return const Scaffold(
-        body: ,
+    return Scaffold(
+        body:Column(
+
+          children: [
+            Image.asset(Assets.images.a.path),
+          ],
+        ),
     );
   }
 
