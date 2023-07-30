9010d189e949e408486ab9de014771180499215a
mahdiramazani <mahdiramazani1281@gmail.com>
2023-07-30 17:13:19 +0330

    add page splacg screean to project

---
+++ lib/splach_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class splach_screan extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
