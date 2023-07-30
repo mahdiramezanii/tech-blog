ac4c022d015258cb02e90cf7df4ae80817b2bf92
mahdiramazani <mahdiramazani1281@gmail.com>
2023-07-30 17:18:00 +0330

    add static files in pubspc.yml

--- lib/splach_screen.dart
+++ lib/splach_screen.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class splach_screan extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    
+    return Scaffold(
+      body: Column(
+          children: [
+            Image.asset(),
+          ],
+      ),
+    )
   }
 
 
