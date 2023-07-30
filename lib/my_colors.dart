56bdaf51c0bdb4a25f17179e7ef376b7a3ac5497
mahdiramazani <mahdiramazani1281@gmail.com>
2023-07-30 17:11:37 +0330

    add gradina colo

--- lib/my_colors.dart
+++ lib/my_colors.dart
@@ -5,3 +5,10 @@ class SolidCollors{
   static const Color primayColo=Color.fromARGB(255, 66, 4, 87);
 
 }
+
+class GradinatCollors {
+  
+  static const List<Color> buttonNavigation=[
+    Color.fromARGB(255, 27, 0, 94),
+  ];
+}
