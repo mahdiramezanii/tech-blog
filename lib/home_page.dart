a8e01f86b14bf74eb1aad79cf281a664b691b113
mahdiramazani <mahdiramazani1281@gmail.com>
2023-07-31 17:34:36 +0330

    fix a bug

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -88,21 +88,29 @@ class HomePage extends StatelessWidget {
                 )
               ],
             ),
-            Container(
-              height: 60,
-              width: 400,
-              child: ListView.builder(
-                  itemCount: 20,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 20,
-                      width: 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.black),
-                    );
-                  }),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,10,0,0),
+              child: Container(
+                height: 30,
+                width: 400,
+                child: ListView.builder(
+                    itemCount: 20,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(5,0,5,0),
+                        child: Container(
+                          height: 20,
+                          width: 60,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.black),
+                      
+                              child: Center(child: Text("data",style: TextStyle(color: Colors.white),)),
+                        ),
+                      );
+                    }),
+              ),
             )
           ],
         ),
