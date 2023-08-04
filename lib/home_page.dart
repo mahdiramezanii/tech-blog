447ecdc26f328c05821f8b5044549e4f31302e3e
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-04 23:16:28 +0330

    fix a bug

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,187 +15,193 @@ class HomePage extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
-          body: Column(
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-          //header applixation
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0,10,0,0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+          body: SingleChildScrollView(
+            child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            //header applixation
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,10,0,0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Icon(Icons.menu,size: 35,),
+                  SizedBox(
+                      height: size.height/13.6,
+                      width: size.width/3.7,
+                      child: Image(image: AssetImage(Assets.images.headicon.path))),
+                  const Icon(Icons.search,size: 35,)
+                ],
+              ),
+            ),
+            //image poster
+            Stack(
               children: [
-                const Icon(Icons.menu,size: 35,),
-                SizedBox(
-                    height: size.height/13.6,
-                    width: size.width/3.7,
-                    child: Image(image: AssetImage(Assets.images.headicon.path))),
-                const Icon(Icons.search,size: 35,)
+                //Cintainer for image in header
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0,8,0,0),
+                  child: Container(
+                    height: size.height/4.2,
+                    width: size.width/1.19,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      
+                      image: DecorationImage(image: AssetImage(Assets.images.programming.path),
+                      fit: BoxFit.cover),
+                  
+                    ),
+                    foregroundDecoration:const  BoxDecoration(
+                            borderRadius: const BorderRadius.all(Radius.circular(16)),
+                            gradient: LinearGradient(colors: GradinatCollors.homeCoverImage,
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter
+                            ),
+                            
+                    ),
+                  ),
+                ),
+                //Text in Image 
+                Positioned(
+                  left: 5,
+                  bottom: 5,
+                  right: 5,
+                  child: Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("ملیکا عزیزی",style: Theme.of(context).textTheme.headline1,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text("250 ",style: Theme.of(context).textTheme.headline1,),
+                            Icon(Icons.remove_red_eye,color: Colors.white,)
+                          ],
+                        ),
+                        
+                      ],
+                    ),
+                    Text("دوازده قدم برنامه نویسی یک دوره ی...س",style:Theme.of(context).textTheme.headline2)
+                  ],),
+                )
               ],
             ),
-          ),
-          //image poster
-          Stack(
-            children: [
-              //Cintainer for image in header
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0,8,0,0),
-                child: Container(
-                  height: size.height/4.2,
-                  width: size.width/1.19,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(16)),
-                    
-                    image: DecorationImage(image: AssetImage(Assets.images.programming.path),
-                    fit: BoxFit.cover),
-                
-                  ),
-                  foregroundDecoration:const  BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(16)),
-                          gradient: LinearGradient(colors: GradinatCollors.homeCoverImage,
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter
-                          ),
-                          
-                  ),
-                ),
-              ),
-              //Text in Image 
-              Positioned(
-                left: 5,
-                bottom: 5,
-                right: 5,
-                child: Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text("ملیکا عزیزی",style: Theme.of(context).textTheme.headline1,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //Hashatg List
+            SizedBox(height: size.height/13,),
+            SizedBox(
+              height: size.height/18,
+          
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: category_item.length,
+                itemBuilder: (BuildContext context , int index){
+          
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(0,0,index==0?size.width/12:20,0),
+                  child: Container(
+                    height: size.height/18,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: const Color.fromARGB(255, 18, 14, 14)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text("250 ",style: Theme.of(context).textTheme.headline1,),
-                          Icon(Icons.remove_red_eye,color: Colors.white,)
-                        ],
-                      ),
-                      
-                    ],
+                        ImageIcon(AssetImage(Assets.icons.hashtag.path),color: Colors.white,size: 10,),
+                        Text("  ${category_item[index].name!} ",style: Theme.of(context).textTheme.headline1),
+                        
+                      ]),
+                    ),
                   ),
-                  Text("دوازده قدم برنامه نویسی یک دوره ی...س",style:Theme.of(context).textTheme.headline2)
-                ],),
-              )
-            ],
-          ),
-          //Hashatg List
-          SizedBox(height: size.height/13,),
-          SizedBox(
-            height: size.height/18,
-
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: category_item.length,
-              itemBuilder: (BuildContext context , int index){
-
-              return Padding(
-                padding: EdgeInsets.fromLTRB(0,0,index==0?size.width/12:20,0),
-                child: Container(
-                  height: size.height/18,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: const Color.fromARGB(255, 18, 14, 14)
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4),
+                );
+              }),
+            ),
+            //body
+            Column(
+              children: [
+                //View Hot Text
+                Padding(
+                  padding: EdgeInsets.only(right: size.width/12),
+                  child: SizedBox(
+                    height: size.height/8,
+                    width: size.width,
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                      ImageIcon(AssetImage(Assets.icons.hashtag.path),color: Colors.white,size: 10,),
-                      Text("  ${category_item[index].name!} ",style: Theme.of(context).textTheme.headline1),
-                      
-                    ]),
+                      ImageIcon(AssetImage(Assets.icons.pen.path),color: Colors.blue,),
+                      Text(" مشاهده داغترین نوشته ها",style: Theme.of(context).textTheme.headline3,),
+                    ],),
                   ),
                 ),
-              );
-            }),
-          ),
-          //body
-          Column(
-            children: [
-              //View Hot Text
-              Padding(
-                padding: EdgeInsets.only(right: size.width/12),
-                child: SizedBox(
-                  height: size.height/8,
-                  width: size.width,
-                  child: Row(
-                    children: [
-                    ImageIcon(AssetImage(Assets.icons.pen.path),color: Colors.blue,),
-                    Text(" مشاهده داغترین نوشته ها",style: Theme.of(context).textTheme.headline3,),
-                  ],),
-                ),
-              ),
-              //List View Slider Hot Text
-              SizedBox(
-                height:size.height/3.7,
-                
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context,int index){
-
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Stack(
-                          children:[ 
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(size.width/18,0,index==0?size.width/12:0,0),
-                            child: Container(
-                              height:size.height/4.37,
-                              width: size.width/2.50,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                image: DecorationImage(
-                                image: AssetImage(Assets.images.slider.path),
-                                fit: BoxFit.cover )
-                              ),
-                              foregroundDecoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                gradient: LinearGradient(colors: GradinatCollors.listViewHotText,
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter
-                                )
+                //List View Slider Hot Text
+                SizedBox(
+                  height:size.height/3.2,
+                  
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (BuildContext context,int index){
+          
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Stack(
+                            children:[ 
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(size.width/18,0,index==0?size.width/12:0,0),
+                              child: Container(
+                                height:size.height/4.6,
+                                width: size.width/2.50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  image: DecorationImage(
+                                  image: AssetImage(Assets.images.slider.path),
+                                  fit: BoxFit.cover )
+                                ),
+                                foregroundDecoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  gradient: LinearGradient(colors: GradinatCollors.listViewHotText,
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter
+                                  )
+                                ),
                               ),
                             ),
+                            Positioned(
+                              bottom: 5,
+                              left: 30,
+                              right: 10,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                Text("ملیکا عزیزی",style: Theme.of(context).textTheme.headline1,),
+                                Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text("250 ",style: Theme.of(context).textTheme.headline1,),
+                            Icon(Icons.remove_red_eye,color: Colors.white,)
+                          ],
+                        ),
+                              ],),
+                            ),
+                        ]),
+                        SizedBox(
+                         width: size.width/2.50,
+                          child: const Text("منام بتببببالاسیلایسایااببetytghtghghhfhfhfhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhfyyyyyyyyyyyyyyyyyyyyyباایبایبایانویس جنگو",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
                           ),
-                          Positioned(
-                            bottom: 5,
-                            left: 30,
-                            right: 10,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                              Text("ملیکا عزیزی",style: Theme.of(context).textTheme.headline1,),
-                              Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text("250 ",style: Theme.of(context).textTheme.headline1,),
-                          Icon(Icons.remove_red_eye,color: Colors.white,)
+                        )
                         ],
-                      ),
-                            ],),
-                          ),
-                      ]),
-                      Text("منام نویس جنگو",
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 5,)
-                      ],
-                    );
-                  }),
-              )
-            ],
-          )
-
-        ],
-      )),
+                      );
+                    }),
+                )
+              ],
+            )
+          
+                  ],
+                ),
+          )),
     );
   }
 }
