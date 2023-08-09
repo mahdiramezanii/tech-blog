797aaa593c3f1be1a37575a27096947548bfc108
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-09 11:58:55 +0330

    edit file my string

--- lib/my_string.dart
+++ lib/my_string.dart
@@ -1,4 +1,8 @@
 class MyString{
   static const String sendPadcast="ارسال پادکست";
   static const String viewHotText="مشاهده داغترین نوشته ها";
+  static const String myFavoratearticleString="مقاله های مورد علاقه من";
+  static const String myFavoratePdcastString="پادکست های مورد علاقه من";
+  static const String logOutMyAcount="خروج از حساب کاربری";
+  static const String editImageProfilestring="ویرایش عکس پروفایل";
 }
