ddd50bc495052adc0677ba2fe414ad961ffbb88f
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-15 18:46:44 +0330

    add New Packge to project

---
+++ lib/views/welcome_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomPageView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const SafeArea(
+      child: Scaffold(
+        body:Column(
+
+            
+        ) ,
+        ) );
+  }
+
+  
+}
