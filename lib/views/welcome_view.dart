7f5a38845d3acc230d1b30a972d829a0d36fe2a8
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-30 18:07:16 +0330

    fix a bug im screan View

--- lib/views/welcome_view.dart
+++ lib/views/welcome_view.dart
@@ -37,66 +37,7 @@ class WelcomPageView extends StatelessWidget{
              ElevatedButton(
               onPressed: (){
 
-              showModalBottomSheet(
-              isScrollControlled: true,
-              backgroundColor:Colors.transparent,
-              context: context, builder: (BuildContext context){
-                
-                return Padding(
-                  padding:  EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height/3,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(20)),
-                      color: Colors.white,
-                    ),
-                
-                    child: Column(
-                      
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding:  EdgeInsets.only(bottom: 10),
-                          child: Text("ایمیل  خودت رو وارد کن",style: TextStyle(fontSize: 20,
-                          fontFamily: "dana",
-                          fontWeight: FontWeight.w300 ),),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: TextField(
-                            onChanged:(value){
-                             
-                              print(isEmail(value));
-                            },
-                            
-                            textAlign: TextAlign.left,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
-                                borderSide: BorderSide(width: 10)
-                              ),
-                              hintText: "mahdiramazani@gmail.com",
-                              
-                              hintStyle: TextStyle(
-                                color: Colors.black,
-                                fontFamily: "dana",
-                                fontSize: 18,
-                                fontWeight: FontWeight.w300
-                              )
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0,20,0,0),
-                          child: ElevatedButton(onPressed: (){},
-                           child: Text("بزن بریم"),
-                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                );
-              });
+              _showBottunNavigation(context);
               },
               child: Text("ثبت نام"),
               style: ButtonStyle(
@@ -116,6 +57,69 @@ class WelcomPageView extends StatelessWidget{
           ),
         ) ,
         ) );
+  }
+
+  Future<dynamic> _showBottunNavigation(BuildContext context) {
+    return showModalBottomSheet(
+            isScrollControlled: true,
+            backgroundColor:Colors.transparent,
+            context: context, builder: (BuildContext context){
+              
+              return Padding(
+                padding:  EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: Container(
+                  height: MediaQuery.of(context).size.height/3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(20)),
+                    color: Colors.white,
+                  ),
+              
+                  child: Column(
+                    
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding:  EdgeInsets.only(bottom: 10),
+                        child: Text("ایمیل  خودت رو وارد کن",style: TextStyle(fontSize: 20,
+                        fontFamily: "dana",
+                        fontWeight: FontWeight.w300 ),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: TextField(
+                          onChanged:(value){
+                           
+                            print(isEmail(value));
+                          },
+                          
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide(width: 10)
+                            ),
+                            hintText: "mahdiramazani@gmail.com",
+                            
+                            hintStyle: TextStyle(
+                              color: Colors.black,
+                              fontFamily: "dana",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300
+                            )
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0,20,0,0),
+                        child: ElevatedButton(onPressed: (){},
+                         child: Text("بزن بریم"),
+                         ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            });
   }
 
   
