1eaf515373398a7d38d1fd578cd18318f7238d17
mahdiramazani <mahdiramazani1281@gmail.com>
2023-09-05 21:50:11 +0330

    complate model Top Visited

--- lib/models/home_models.dart
+++ lib/models/home_models.dart
@@ -1,6 +1,44 @@
 class TopVisited{
 
+  String? id;
+  String? title;
+  String? image;
+  String? cat_id;
+  String? author;
+  String? view;
+  String? status;
 
 
-  
+  TopVisited(
+    {
+      required this.id,
+      required this.title,
+      required this.image,
+      required this.cat_id,
+      required this.author,
+      required this.view,
+      required this.status
+    }
+  )
+
+  TopVisited.fromJson(Map<String,dynamic> element{
+
+    
+
+  } )
+
+
+
 }
+
+        // {
+        //     "id": "1",
+        //     "title": "۵ بازی مشابه Assassin’s Creed Valhalla که باید بازی کنید",
+        //     "image": "/Techblog/assets/upload/images/article/valhalla.jpg",
+        //     "cat_id": "1",
+        //     "cat_name": "اخبار و مقالات",
+        //     "author": "بهرام امینی",
+        //     "view": "2990",
+        //     "status": "1",
+        //     "created_at": "۱۴۰۱/۲/۱۶"
+        // },
