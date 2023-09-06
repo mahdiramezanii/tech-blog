6e057d5b480d84303d60445c34ea8ee8335b1fb3
mahdiramazani <mahdiramazani1281@gmail.com>
2023-09-06 10:38:35 +0330

    add dio service

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,4 +1,18 @@
-class Dio{
+import 'package:dio/dio.dart';
 
-  
+class DioSerice{
+
+  Dio dio=Dio();
+
+  Future<dynamic> getMethod(String url) async{
+
+
+    return await dio.get(
+      url,options: Options(
+        responseType: ResponseType.json,method: "Get" 
+      )).then((response) {
+
+        
+      });
+  }
 }
