8f0271c0042abc5eaaff415605fa16c59d610183
mahdiramazani <mahdiramazani1281@gmail.com>
2023-09-06 11:48:43 +0330

    edit controller

--- lib/constant/api_constant.dart
+++ lib/constant/api_constant.dart
@@ -2,6 +2,6 @@ class ApiUrlConstant{
 
   static const hostUrl="https://techblog.sasansafari.com";
   static const BaseUrl="https://techblog.sasansafari.com/Techblog/api/";
-
+  static const get_home_items=BaseUrl+"home/?command=index";
 
 }
